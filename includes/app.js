c8cc98d2389e85dcc6a5d7fc518846c01ebb579b
Zed <willem.zheng.xu@gmail.com>
2021-03-24 23:50:50 +0100

    REDO

---
+++ includes/app.js
@@ -0,0 +1,15 @@
+// Da Vinci Front-End Challenge Stemwijzer Willem Xu
+
+// Laat alle subjects zien in variable: subjects in parties.js file
+subjects.forEach(function(subject){ 
+    console.log(subject)
+    }
+);
+
+
+// Maak variables
+let count = 0
+const title = document.getElementById("statementTitle");
+const statement = document.getElementById("statementState");
+const backButton = document.getElementById("backButton");
+const buttonContainer = document.getElementById("")
